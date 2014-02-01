28ab5ead31466c357082c26e5051399c11407472
Jesse Lee <jessehlee@gmail.com>
2014-02-01 12:42:28 -0500

    hw2 first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hw2cis::Application.config.secret_key_base = 'ffb9682c864bae9cffca48745e55266144e948ffc31f13f199bee14a804c54eef56340ac52422f94ce3718997100666cf48299b971fa49ad73ebd83d6ff44c6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hw2cis::Application.config.session_store :cookie_store, key: '_hw2cis_session'
